727d4244ff9b5569c35d970e76ad53e11e6a7c49
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-08-21 14:50:48 +0800

    added missing space in call to cmake

--- build_imager.sh
+++ build_imager.sh
@@ -93,7 +93,7 @@ echo "Building the software.."
 
 [ -d ${build_dir} ] || mkdir ${build_dir}
 cd ${build_dir}
-cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_CXX_COMPILER=hipcc -DUSE_HIP=ON  -DCMAKE_BUILD_TYPE=Release-DCMAKE_CXX_FLAGS=-O3 ${cmake_options}
+cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_CXX_COMPILER=hipcc -DUSE_HIP=ON  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-O3 ${cmake_options}
 make -j 12 VERBOSE=1
 make test
 # Install the software
